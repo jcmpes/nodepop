1c55cf9ebd61f4d9f2afceb03972966aa12a2d37
jc <jcmpes@gmail.com>
2021-02-20 10:55:41 +0100

    Added views.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { postView } from './views.js'
+
 window.addEventListener('DOMContentLoaded', async (e) => {
     async function loadPosts() {
         const url = 'http://localhost:8000/api/posts'
@@ -11,39 +13,17 @@ window.addEventListener('DOMContentLoaded', async (e) => {
         }
     }
 
-    function render(posts) {
+    function render(posts, type) {
         const postList = document.querySelector('.main');
         for (const post of posts) {
-            const article = document.createElement('article');
-            article.innerHTML = `
-            <div class="card">
-                <div class="card-image">
-                    <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                    <div class="media-content">
-                        <p class="title is-4">${post.title}</p>
-                        <p class="subtitle is-6">@${post.author}</p>
-                    </div>
-                    </div>
-
-                    <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
-                    <a href="#">#css</a> <a href="#">#responsive</a>
-                    <br>
-                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                    </div>
-                </div>
-            </div>
-            `
-            postList.appendChild(article);
+            if (post.type = type) {
+                const article = document.createElement('article');
+                article.innerHTML = postView(post)
+                postList.appendChild(article);
+            }
         }
     }
 
     const posts = await loadPosts()
-    render(posts)
+    render(posts, "Venta")
 })
---
+++ views.js
@@ -0,0 +1,28 @@
+export const postView = (post) => {
+    return `
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                <div class="media-content">
+                    <p class="title is-3">${post.price}€</p>
+                    <p class="title is-4">${post.title}</p>
+                    <p class="subtitle is-6">@${post.author}</p>
+                </div>
+                </div>
+
+                <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris.
+                <a href="#">#css</a> <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1"></time>
+                </div>
+            </div>
+        </div>
+    `
+}
